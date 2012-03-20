fcdaac4121a060e371ab4f4d8685161626c3233b
Dmitry A. Novikov <Dmitry.A.Novikov@gmail.com>
2012-03-20 19:27:16 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoThereIDontKnowWhere::Application.config.secret_token = '38dff3823e7902f5674f2cca8f811fd3985cceaaa17d892c440b898005208425992518fc1095f25585f3d090572ec981747aa7f1c2c90ac6c6d1452183f00f1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GoThereIDontKnowWhere::Application.config.session_store :cookie_store, key: '_go_there_I_dont_know_where_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GoThereIDontKnowWhere::Application.config.session_store :active_record_store
